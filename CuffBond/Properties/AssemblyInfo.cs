6ce581ab1dcac3b31e4a18391784e6163bda90ed
MrDarvi <darviyt3@gmail.com>
2026-05-18 16:16:05 +0300

    Add files via upload

---
+++ CuffBond/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CuffBond")]
+[assembly: AssemblyDescription("Improved plugin for linking allies for SCP:SL - EXILED framework")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MisterDarvi")]
+[assembly: AssemblyProduct("CuffBond")]
+[assembly: AssemblyCopyright("Copyright © MisterDarvi 2026")]
+[assembly: AssemblyTrademark("MisterDarvi")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c4823500-4315-4401-8cd0-dc2a0351b05c")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
