20f3e8b5cd1ab5d4fd2f08b2212f70676f0fc391
MrDarvi <darviyt3@gmail.com>
2026-05-18 16:18:13 +0300

    Add files via upload

---
+++ CuffBond/Handlers/ServerHandlers.cs
@@ -0,0 +1,10 @@
+﻿using CuffBond.Core;
+using Exiled.Events.EventArgs.Server;
+
+namespace CuffBond.Handlers
+{
+    public class ServerHandlers
+    {
+        public void OnRoundEnded(RoundEndedEventArgs ev) => CuffManager.Clear();
+    }
+}
